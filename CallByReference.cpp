fd6beb321c52da24257cb48b3126ab782602d9d9
Basu5499 <basuck143@gmail.com>
2024-06-11 18:07:47 +0530

    Second Update

--- CallByReference.cpp
+++ CallByReference.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *x,int *y)
+void swap(int &x,int &y)
 {
-    int temp=*x;
-    *x=*y;
-    *y=temp;
+    int temp=x;
+    x=y;
+    y=temp;
 }
 
 int main()
 {
     int a=34,b=54;
     cout<<"Before Swap"<<endl<<"a = "<<a<<" b = "<<b<<endl;
-    swap(&a,&b);
+    swap(a,b);
     cout<<"After Swap"<<endl<<"a = "<<a<<" b = "<<b<<endl;
     return 0;
 }
